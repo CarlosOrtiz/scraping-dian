fa95de1bd4ac5de1aa9f3e90d8accdb0c5a766d4
Zeroga98 <sebastianrojasgarzon@hotmail.com>
2020-08-24 10:11:27 -0500

    feat(typeorm): ssl

--- src/@common/config/typeorm.config.ts
+++ src/@common/config/typeorm.config.ts
@@ -1,17 +1,20 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs('typeorm', () => {
-  
+
   const configDefault = {
     type: process.env.DB_TYPE,
     host: process.env.DB_HOST,
-    port: +process.env.DB_PORT,
+    port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     synchronize: true,
     logging: false,
-    entities: ['dist/entities/**/*entity.js']
+    entities: ['dist/entities/**/*entity.js'],
+    ssl:  {
+      rejectUnauthorized: false
+    }
     //entities: [(process.env.NODE_ENV === 'local' ? 'src/entities/**/*.ts' : 'dist/entities/**/*.js' )]
   }
 
